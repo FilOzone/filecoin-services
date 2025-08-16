8b5219149e0623c69738fce440e3e82d7e5ace03
William Morriss <wjmelements@gmail.com>
2025-08-15 21:19:06 -0500

    wip autogenerate src/FilecoinWarmStorageServiceView.sol

---
+++ service_contracts/tools/generate_view_contract.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo // SPDX-License-Identifier: Apache-2.0 OR MIT
+echo pragma solidity ^0.8.20\;
+echo
+echo // Generated with $0 $@
+echo
+
+echo 'import "./FilecoinWarmStorageService.sol";'
+echo 'import "./lib/WarmStorageView.sol";'
+
+echo contract FilecoinWarmStorageServiceView {
+echo "    using WarmStorageView for FilecoinWarmStorageService;"
+echo "    FilecoinWarmStorageService public immutable service;"
+echo "    constructor(FilecoinWarmStorageService _service) {"
+echo "        service = _service;"
+echo "    }"
+
+jq -rM 'reduce .abi.[] as {$type,$name,$inputs,$outputs} (
+    null;
+    if $type == "function"
+    then
+        . += "    function " + $name + "(" +
+            ( reduce $inputs.[] as {$type,$name} (
+                [];
+                if $type != "FilecoinWarmStorageService"
+                then
+                    . += [$type + " " + $name]
+                end
+            ) | join(", ") ) +
+        ") external view returns (" +
+            ( reduce $outputs.[] as {$type,$name,$internalType} (
+                []; 
+                if $name == ""
+                then
+                    if $type == "tuple" or $type == "tuple[]"
+                    then
+                        . += [( $internalType | .[7:] )]
+                    else
+                        . += [$type]
+                    end
+                else
+                    if $type == "tuple" or $type == "tuple[]"
+                    then
+                        . += [($internalType | .[7:]) + " " + $name]
+                    else
+                        . += [$type + " " + $name]
+                    end
+                end
+            ) | join(", ") ) +
+        ") {\n        return service." + $name + "(" +
+            ( reduce $inputs.[] as {$name,$type} (
+                [];
+                if $type != "FilecoinWarmStorageService"
+                then
+                    . += [$name]
+                end
+            ) | join(", ") ) +
+        ");\n    }\n"
+    end
+    )' $1
+
+echo }
