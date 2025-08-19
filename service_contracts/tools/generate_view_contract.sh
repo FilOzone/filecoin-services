f41c73a610555f6c1a78d915762effd1f306e8e2
William Morriss <wjmelements@gmail.com>
2025-08-19 11:24:35 -0500

    update generate_view_contract to use StateInternalLibrary

--- service_contracts/tools/generate_view_contract.sh
+++ service_contracts/tools/generate_view_contract.sh
@@ -7,10 +7,10 @@ echo // Generated with $0 $@
 echo
 
 echo 'import "./FilecoinWarmStorageService.sol";'
-echo 'import "./lib/FilecoinWarmStorageServiceStateLibrary.sol";'
+echo 'import "./lib/FilecoinWarmStorageServiceStateInternalLibrary.sol";'
 
 echo contract FilecoinWarmStorageServiceStateView {
-echo "    using FilecoinWarmStorageServiceStateLibrary for FilecoinWarmStorageService;"
+echo "    using FilecoinWarmStorageServiceStateInternalLibrary for FilecoinWarmStorageService;"
 echo "    FilecoinWarmStorageService public immutable service;"
 echo "    constructor(FilecoinWarmStorageService _service) {"
 echo "        service = _service;"
