47ef9d0deea5e6bd208c9a6703f1f0bd6bde5be3
Rod Vagg <rod@vagg.org>
2025-08-20 19:30:58 +1000

    chore: adapt metadata to extload changes

--- service_contracts/src/FilecoinWarmStorageServiceStateView.sol
+++ service_contracts/src/FilecoinWarmStorageServiceStateView.sol
@@ -47,8 +47,32 @@ contract FilecoinWarmStorageServiceStateView {
         return service.getMaxProvingPeriod();
     }
 
-    function getPieceMetadata(uint256 dataSetId, uint256 pieceId) external view returns (string memory) {
-        return service.getPieceMetadata(dataSetId, pieceId);
+    function getDataSetMetadata(uint256 dataSetId, string memory key) external view returns (string memory) {
+        return service.getDataSetMetadata(dataSetId, key);
+    }
+
+    function getAllDataSetMetadata(uint256 dataSetId)
+        external
+        view
+        returns (string[] memory keys, string[] memory values)
+    {
+        return service.getAllDataSetMetadata(dataSetId);
+    }
+
+    function getPieceMetadata(uint256 dataSetId, uint256 pieceId, string memory key)
+        external
+        view
+        returns (string memory)
+    {
+        return service.getPieceMetadata(dataSetId, pieceId, key);
+    }
+
+    function getAllPieceMetadata(uint256 dataSetId, uint256 pieceId)
+        external
+        view
+        returns (string[] memory keys, string[] memory values)
+    {
+        return service.getAllPieceMetadata(dataSetId, pieceId);
     }
 
     function initChallengeWindowStart() external view returns (uint256) {
