3122679479e302a3037450acefa05a7aa414b6b9
Jakub Sztandera <oss@kubuxu.com>
2026-08-21 16:44:35 +0200

    chore: prep FWSS v1.4.0 release
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- service_contracts/test/FilecoinWarmStorageService.t.sol
+++ service_contracts/test/FilecoinWarmStorageService.t.sol
@@ -5863,7 +5863,7 @@ contract FilecoinWarmStorageServiceUpgradeTest is Test {
             if (logs[i].topics[0] == expectedTopic) {
                 // Decode and verify the event data
                 (string memory version, address implementation) = abi.decode(logs[i].data, (string, address));
-                assertEq(version, "1.3.1", "Version should be 1.3.1");
+                assertEq(version, "1.4.0", "Version should be 1.4.0");
                 assertTrue(implementation != address(0), "Implementation address should not be zero");
                 foundEvent = true;
                 break;
